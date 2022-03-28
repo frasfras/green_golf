a17109abe6c26066c3d8b0eea8f9081bc93a2972
frasfras <65541080+frasfras@users.noreply.github.com>
2022-03-28 09:04:56 +0800

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    
+    <HelloWorld msg="Water Plan  "/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/CoursesWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
